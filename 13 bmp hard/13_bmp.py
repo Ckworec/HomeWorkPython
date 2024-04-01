20f5e9a38d1a01f3344ef8b81f0b1a0b5984c845
Ckworec <114090016+Ckworec@users.noreply.github.com>
2024-04-01 09:49:31 +0300

    <3

--- 13 bmp hard/13_bmp.py
+++ 13 bmp hard/13_bmp.py
@@ -8,28 +8,64 @@ x1, y1 = imin.size
 px1 = imin.load()
 
 imout = Image.new("RGB", (x1, y1), (0, 0, 0))
+draw = ImageDraw.Draw(imout)
 pxout = imout.load()
 
-for i in range(x1):
-	for j in range(y1):
-		r1 = px1[i, j][0]
-		g1 = px1[i, j][1]
-		b1 = px1[i, j][2]
-		compression = 0.9 #сжатие
-		n = 1 - compression
+size_square = 15
+fractal = 0
+
+for i in range(0, x1, size_square):
+	for j in range(0, y1, size_square):
+		if i + 2 * size_square - 1 < x1 and j + 2 * size_square - 1 < y1:
+			for k in range(size_square):
+				for t in range(size_square):
+					r = px1[i + k, j + t][0]
+					g = px1[i + k, j + t][1]
+					b = px1[i + k, j + t][2]
+
+					fractal += (r + g + b) // 3
 		
-		if i < x1 // 2:
-			i1 = int(i ** n / (x1 // 2) ** (n - 1))
-			i2 = int((i + 1) ** n / (x1 // 2) ** (n - 1))
-			
-			for k in range(i1, i2):
-				if k < x1 // 2:
-					pxout[k, j] = r1, g1, b1
+			fractala = [0] * 3
+
+			for k in range(size_square):
+				for t in range(size_square):
+					r = px1[i + size_square + k, j + t][0]
+					g = px1[i + size_square + k, j + t][1]
+					b = px1[i + size_square + k, j + t][2]
+
+					fractala[0] += (r + g + b) // 3
+
+			for k in range(size_square):
+				for t in range(size_square):
+					r = px1[i + k, j + size_square + t][0]
+					g = px1[i + k, j + size_square + t][1]
+					b = px1[i + k, j + size_square + t][2]
+
+					fractala[1] += (r + g + b) // 3
+
+			for k in range(size_square):
+				for t in range(size_square):
+					r = px1[i + size_square + k, j + size_square + t][0]
+					g = px1[i + size_square + k, j + size_square + t][1]
+					b = px1[i + size_square + k, j + size_square + t][2]
+
+					fractala[2] += (r + g + b) // 3
+
+			min_difference = min(fractala)
+
+			index = fractala.index(min_difference)
+
+			for k in range(2 * size_square):
+				for t in range(2 * size_square):
+					if index == 0:
+						draw.point((i + k, j + t), (px1[i + size_square + k % size_square, j + t % size_square][0], px1[i + size_square + k % size_square, j + t % size_square][1], px1[i + size_square + k % size_square, j + t % size_square][2]))
+					elif index == 1:
+						draw.point((i + k, j + t), (px1[i + k % size_square, j + size_square + t % size_square][0], px1[i + k % size_square, j + size_square + t % size_square][1], px1[i + k % size_square, j + size_square + t % size_square][2]))
+					else:
+						draw.point((i + k, j + t), (px1[i + size_square + k % size_square, j + size_square + t % size_square][0], px1[i + size_square + k % size_square, j + size_square + t % size_square][1], px1[i + size_square + k % size_square, j + size_square + t % size_square][2]))
 		else:
-			i1 = int((x1 - i - 1) ** n / (x1 // 2) ** (n - 1))
-			i2 = int((x1 - i) ** n / (x1 // 2) ** (n - 1))
-			
-			for k in range(x1 - i2,x1 - i1):
-				pxout[k, j] = r1, g1, b1
+			for k in range(x1 - i):
+				for t in range(y1 - j):
+					draw.point((i + k, j + t), (px1[i + k, j + t][0], px1[i + k, j + t][1], px1[i + k, j + t][2]))
 
 imout.save("out.bmp", "BMP")
