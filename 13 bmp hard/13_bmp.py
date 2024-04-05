b0a2299c2fc3369746511c2d7bf77a99c2811544
Ckworec <114090016+Ckworec@users.noreply.github.com>
2024-04-05 08:13:47 +0300

    <3

--- 13 bmp hard/13_bmp.py
+++ 13 bmp hard/13_bmp.py
@@ -1,71 +1,99 @@
-import sys
-from PIL import Image, ImageDraw
+import numpy as np
+from skimage import io, util, measure
+import matplotlib.pyplot as plt
+from PIL import Image 
 
-imin = Image.open("input.bmp")
+"""
+tolerance - означает допустимое отклонение при сравнении блоков изображения в процессе сжатия и декомпрессии. 
+Значение tolerance определяет, насколько похожи блоки должны быть, 
+чтобы считаться одинаковыми и заменяться друг другом в процессе сжатия и декомпрессии. 
+Уменьшение значения tolerance приведет к более точному сжатию изображения, 
+но может увеличить размер результирующего файла. Увеличение значения tolerance, 
+наоборот, может уменьшить размер результирующего файла, 
+но может привести к некоторой потере качества изображения.
+"""
 
-x1, y1 = imin.size
+def fractal_compress(image, tolerance):
+    # Сконвертировать изображение в черно-белое
+    image_gray = util.img_as_float(image)
+    if len(image_gray.shape) > 2:
+        image_gray = util.img_as_float(image_gray[:,:,0])
 
-px1 = imin.load()
+    # Получить количество блоков и их размер
+    block_size = 4
+    width, height = image_gray.shape
+    num_blocks = (width // block_size) * (height // block_size)
 
-imout = Image.new("RGB", (x1, y1), (0, 0, 0))
-draw = ImageDraw.Draw(imout)
-pxout = imout.load()
+    # Разбить изображение на блоки
+    blocks = image_gray[:width - width % block_size, :height - height % block_size].reshape(width // block_size, height // block_size, block_size, block_size)
 
-size_square = 15
-fractal = 0
+    # Найти среднее значение каждого блока
+    # Заполняем массив нулями
+    block_means = np.zeros((width // block_size, height // block_size))
 
-for i in range(0, x1, size_square):
-	for j in range(0, y1, size_square):
-		if i + 2 * size_square - 1 < x1 and j + 2 * size_square - 1 < y1:
-			for k in range(size_square):
-				for t in range(size_square):
-					r = px1[i + k, j + t][0]
-					g = px1[i + k, j + t][1]
-					b = px1[i + k, j + t][2]
+    for i in range(width // block_size):
+        for j in range(height // block_size):
+            block_means[i, j] = np.mean(blocks[i, j]) # Среднее арифметическое блока
 
-					fractal += (r + g + b) // 3
-		
-			fractala = [0] * 3
+    # Найти наиболее схожие блоки
+    similar_blocks = np.zeros((width // block_size, height // block_size), dtype = object) # Узнать что такое object
 
-			for k in range(size_square):
-				for t in range(size_square):
-					r = px1[i + size_square + k, j + t][0]
-					g = px1[i + size_square + k, j + t][1]
-					b = px1[i + size_square + k, j + t][2]
+    for i in range(width // block_size):
+        for j in range(height // block_size):
+            block_diff = np.abs(block_means - block_means[i, j]) # Поэлементное взятие модуля
+            similar_blocks[i, j] = np.argwhere(block_diff <= tolerance).tolist()
 
-					fractala[0] += (r + g + b) // 3
+    compressed_image = np.zeros_like(image_gray)
 
-			for k in range(size_square):
-				for t in range(size_square):
-					r = px1[i + k, j + size_square + t][0]
-					g = px1[i + k, j + size_square + t][1]
-					b = px1[i + k, j + size_square + t][2]
+    for i in range(width // block_size):
+        for j in range(height // block_size):
+            block_indices = similar_blocks[i, j]
+            for index in block_indices:
+                x, y = index
+                compressed_image[i * block_size:(i + 1) * block_size, j * block_size:(j + 1) * block_size] = blocks[x, y]
 
-					fractala[1] += (r + g + b) // 3
+    return compressed_image
 
-			for k in range(size_square):
-				for t in range(size_square):
-					r = px1[i + size_square + k, j + size_square + t][0]
-					g = px1[i + size_square + k, j + size_square + t][1]
-					b = px1[i + size_square + k, j + size_square + t][2]
+def fractal_decompress(compressed_image, original_image, tolerance):
+    # Декомпрессия изображения
+    compressed_image_gray = util.img_as_float(compressed_image)
+    if len(compressed_image_gray.shape) > 2:
+        compressed_image_gray = util.img_as_float(compressed_image_gray[:,:,0])
 
-					fractala[2] += (r + g + b) // 3
+    original_image_gray = util.img_as_float(original_image)
 
-			min_difference = min(fractala)
+    if len(original_image_gray.shape) > 2:
+        original_image_gray = util.img_as_float(original_image_gray[:,:,0])
 
-			index = fractala.index(min_difference)
+    compressed_image = np.zeros_like(original_image_gray)
+    block_size = 4
+    width, height = compressed_image_gray.shape
 
-			for k in range(2 * size_square):
-				for t in range(2 * size_square):
-					if index == 0:
-						draw.point((i + k, j + t), (px1[i + size_square + k % size_square, j + t % size_square][0], px1[i + size_square + k % size_square, j + t % size_square][1], px1[i + size_square + k % size_square, j + t % size_square][2]))
-					elif index == 1:
-						draw.point((i + k, j + t), (px1[i + k % size_square, j + size_square + t % size_square][0], px1[i + k % size_square, j + size_square + t % size_square][1], px1[i + k % size_square, j + size_square + t % size_square][2]))
-					else:
-						draw.point((i + k, j + t), (px1[i + size_square + k % size_square, j + size_square + t % size_square][0], px1[i + size_square + k % size_square, j + size_square + t % size_square][1], px1[i + size_square + k % size_square, j + size_square + t % size_square][2]))
-		else:
-			for k in range(x1 - i):
-				for t in range(y1 - j):
-					draw.point((i + k, j + t), (px1[i + k, j + t][0], px1[i + k, j + t][1], px1[i + k, j + t][2]))
+    for i in range(0, width, block_size):
+        for j in range(0, height, block_size):
+            block = compressed_image_gray[i:i + block_size, j:j + block_size]
+            diff = np.abs(original_image_gray[i:i + block_size, j:j + block_size] - block)
+            if np.mean(diff) <= tolerance:
+                compressed_image[i:i + block_size, j:j + block_size] = block
+            else:
+                compressed_image[i:i + block_size, j:j + block_size] = original_image_gray[i:i + block_size, j:j + block_size]
 
-imout.save("out.bmp", "BMP")
+    return compressed_image
+
+# Загрузить изображение
+image = io.imread('input.bmp')
+
+# Сжать изображение
+compressed_image = fractal_compress(image, tolerance = 0.2)
+
+# Восстановить изображение
+reconstructed_image = fractal_decompress(compressed_image, image, tolerance = 0.2)
+
+# Отобразить изображения
+io.imshow(image)
+
+io.imshow(compressed_image)
+plt.imsave('compress.bmp', compressed_image, cmap='gray')
+
+io.imshow(reconstructed_image)
+plt.imsave('result.bmp', reconstructed_image, cmap='gray')
